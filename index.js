795ab5ea9d9c471d350e52048fa4bb35bf9c6889
ameliebarrero@icloud.com <ameliebarrero@icloud.com>
2018-07-11 20:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your code in this file!
 var currentUser = "Amelie"
 var welcomeMessage = "Welcome to Klossybook, " + (currentUser) + "!"
-const upperCaseCurrentUser = currentuser.toUpperCase();
 var excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, "+ (upperCaseCurrentUser) 
 const upperCaseCurrentUser = currentuser.toUpperCase();
 // + "!"
