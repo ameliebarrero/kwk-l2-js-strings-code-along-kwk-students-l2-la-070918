b9f792450f0bdff5b982a20fc76f94d03d289a84
ameliebarrero@icloud.com <ameliebarrero@icloud.com>
2018-07-11 20:33:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ var excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, "+ (currentUser.toUpperCase(
 // const firstInitial = currentUser[0];
 // const restOfName = currentUser.slice(1);
 // var shortGreeting = "Welcome, " + (firstInitial) + "!"
+console.log(excitedWelcomeMessage)
